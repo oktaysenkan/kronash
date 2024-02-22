a5150e77880c6060f67d88b79056578c2b9cacd4
Turbobot <turbobot@vercel.com>
2024-02-22 03:52:35 +0300

    feat(create-turbo): apply official-starter transform

--- packages/core/src/index.ts
+++
@@ -1,6 +0,0 @@
-import Kronash from './lib/kronash';
-
-export * from './lib/kronash';
-export * from './lib/types';
-
-export default Kronash;
--- packages/react/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './lib/KronashProvider';
-export * from './lib/useKronash';
